cd0810afc4bd39fa2d503d69f5b44354971ad5be
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-09-27 23:39:01 -0300

    feat: functions SignIn/SignUp

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import styled from "styled-components";
 
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}`;
+
 export default function MainPage() {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [pass, setPass] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+    const [load, setLoad] = useState(false)
+    const [emailLogin, setEmailLogin] = useState("");
+    const [passLogin, setPassLogin] = useState("");
+
+    function signIn(e) {
+        e.preventDefault();
+        const promise = axios.post("/signIn", {
+          email: emailLogin,
+          password: passLogin,
+        });
+    
+        promise.then((res) => {
+          const { userId, name, token } = res.data;
+          setUser({ userId, name, token });
+          localStorage.setItem("user", JSON.stringify({ userId, name, token }));
+          navigate("/")
+        });
+    
+        promise.catch((err) => {
+          console.log(err.response.data);
+        });
+      }
+
+    function signUp(e) {
+        e.preventDefault();
+        setLoad(true);
+    
+        if (pass != confirmPass) {
+          alert("As senhas devem ser iguais");
+          setLoad(false);
+          return;
+        }
+    
+        const promise = axios.post("/signUp", {
+          name: name,
+          email: email,
+          password: pass,
+        });
+    
+        promise.then((res) => {
+          alert("Conta criada com sucesso");
+          setLoad(false);
+        });
+    
+        promise.catch((err) => {
+          alert(err.response.data);
+          setLoad(false);
+        });
+      }
   return (
     <>
       <SHeader>
@@ -9,66 +65,78 @@ export default function MainPage() {
       </SHeader>
       <SMainContainer>
         <SignInContainer>
-          <form>
+          <form onSubmit={signIn}>
             <h1>Já possui uma conta?</h1>
             <h1>Entre já!</h1>
             <input
               placeholder="E-mail"
               type="email"
               required
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
+              value={emailLogin}
+              onChange={(e) => setEmailLogin(e.target.value)}
             />
             <input
               placeholder="Senha"
               type="password"
               required
-              //value={pass}
-              //onChange={(e) => setPass(e.target.value)}
+              value={passLogin}
+              onChange={(e) => setPassLogin(e.target.value)}
             />
             <button type="submit">Entrar</button>
           </form>
         </SignInContainer>
         <SignUpContainer>
-          <form>
+          <form onSubmit={signUp}>
             <h1>Não tem uma conta?</h1>
             <h1>Cadastre-se</h1>
             <input
-              //disabled={load}
+              disabled={load}
               placeholder="Nome"
               required
-              // value={name}
-              // onChange={(e) => setName(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
             />
             <input
-              // disabled={load}
+              disabled={load}
               placeholder="E-mail"
               required
               type="email"
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
-              // disabled={load}
+              disabled={load}
               placeholder="Senha"
               required
               type="password"
-              //value={pass}
-              //onChange={(e) => setPass(e.target.value)}
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
               autocomplete="new-password"
             />
             <input
-              // disabled={load}
+              disabled={load}
               placeholder="Confirme a senha"
               required
               type="password"
-              //value={confirmPass}
-              //onChange={(e) => setConfirmPass(e.target.value)}
+              value={confirmPass}
+              onChange={(e) => setConfirmPass(e.target.value)}
               autocomplete="new-password"
             />
-            <button data-test="sign-up-submit" type="submit">
-              Cadastrar
+            <button disabled={load} type="submit">
+            {load ? (
+              <ThreeDots
+                height="40"
+                width="40"
+                radius="9"
+                color="#FFFFFF"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+              />
+            ) : (
+              "Cadastrar"
+            )}
             </button>
           </form>
         </SignUpContainer>
@@ -98,8 +166,7 @@ const SMainContainer = styled.div`
   display: flex;
   justify-content: space-around;
   height: 1200px;
-  background-color: #D9D9D9;;
-  
+  background-color: #d9d9d9;
 `;
 const SignInContainer = styled.div`
   display: flex;
@@ -107,7 +174,7 @@ const SignInContainer = styled.div`
   margin-top: 10%;
   h1 {
     font-family: Bebas Neue;
-    color:#214405;
+    color: #214405;
     font-size: xx-large;
   }
   input {
@@ -139,7 +206,7 @@ const SignInContainer = styled.div`
     border: none;
     width: calc(100% - 50%);
     border-radius: 5px;
-    background-color:#214405;
+    background-color: #214405;
     font-size: 25px;
     color: white;
     cursor: pointer;
@@ -195,7 +262,7 @@ const SignUpContainer = styled.div`
     border-radius: 5px;
     background-color: #214405;
     font-size: 25px;
-    
+
     color: white;
     cursor: pointer;
 
@@ -203,10 +270,10 @@ const SignUpContainer = styled.div`
   }
   a {
     font-family: Bebas Neue;
-    
+
     font-size: 20px;
     line-height: 18px;
-    color:#214405;
+    color: #214405;
     text-decoration: none;
     padding-top: 30px;
   }
